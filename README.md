7689f71fd4dbaf1ecf518c694a78cdbc19d1d8a3
The Mini John <admin@phoenix-prime.gr>
2013-05-16 21:19:44 +0300

    The Mini John App

---
+++ README.md
@@ -0,0 +1,19 @@
+# The Mini John's Simple Devise App
+
+##Introduction
+This simple app includes devise, Bootstrap, Simple Form's and is ready to deploy to Heroku.
+
+##Usage
+Clone the folder, and if you dont have the required Gem's, install them now.
+
+You can run:
+    bundle install
+
+
+Once deployed on Heroku, please run the command for migrating the Database.
+    heroku run rake db:migrate
+
+
+##You're Done
+that's all, have fun experimenting.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Janni::Application.config.secret_token = '50d07f545958899b1d038c46f4bd513819276f26ed6518746f6a903bb4ef19061c6290cd5478b37a81d9f65cb7b91a4029fa827afd23d421ba728883f40ed9b3'
